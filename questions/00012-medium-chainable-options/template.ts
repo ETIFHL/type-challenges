abe750fc3a8447f6aedcab708df41a91fc6422be
ETIFHL <ashetifhl@gmail.com>
2022-06-14 19:54:12 +0800

    00012-medium-chainable-options

--- questions/00012-medium-chainable-options/template.ts
+++ questions/00012-medium-chainable-options/template.ts
@@ -1,4 +1,4 @@
 type Chainable<T = {}> = {
-  option<K extends string, V>(key: K, value: V): Chainable<T & { [P in K]: V }>
+  option<K extends string, V = any>(key: K extends keyof T ? never : K, value: V): Chainable<T & { [P in K]: V }>
   get(): T
 }
