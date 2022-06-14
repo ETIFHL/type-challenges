57e15ca8b6a2b791ca8aeb764f36760188847f7b
ETIFHL <ashetifhl@gmail.com>
2022-06-14 16:47:50 +0800

    00008-medium-readonly-2

--- questions/00008-medium-readonly-2/template.ts
+++ questions/00008-medium-readonly-2/template.ts
@@ -3,3 +3,7 @@ type MyReadonly2<T, K extends keyof T = keyof T> = {
 } & {
   [P in keyof T as P extends K ? never: P]: T[P]
 }
+
+type MyReadonly2_1<T, K extends keyof T = keyof T> = {
+  readonly [P in K]: T[P]
+} & Omit<T, K>
