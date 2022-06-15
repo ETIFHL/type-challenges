2f3cea4b27d6a2db9f1d6454000f90b2eaf7a2cd
ETIFHL <ashetifhl@gmail.com>
2022-06-15 20:11:07 +0800

    00020-medium-promise-all

--- questions/00020-medium-promise-all/template.ts
+++ questions/00020-medium-promise-all/template.ts
@@ -1,1 +1,1 @@
-declare function PromiseAll(values: any): any
+declare function PromiseAll<T extends any[] = any[]>(values: readonly [...T]): Promise<{ [K in keyof T]: T[K] extends Promise<any> ? Awaited<T[K]> : T[K] }>
