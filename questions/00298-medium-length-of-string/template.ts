783dec36a315ff959652fc926973376db8b726a6
ETIFHL <ashetifhl@gmail.com>
2022-06-16 20:26:12 +0800

    00298-medium-length-of-string

--- questions/00298-medium-length-of-string/template.ts
+++ questions/00298-medium-length-of-string/template.ts
@@ -1,1 +1,1 @@
-type LengthOfString<S extends string> = any
+type LengthOfString<S extends string, A extends string[] = []> = S extends `${infer F}${infer Rest}` ? LengthOfString<Rest, [...A, F]> : A['length']
