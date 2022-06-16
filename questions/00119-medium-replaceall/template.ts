ecd91aeb9bf7f4177f92751b4c8cd48134b9e87a
ETIFHL <ashetifhl@gmail.com>
2022-06-16 16:31:27 +0800

    00119-medium-replaceall

--- questions/00119-medium-replaceall/template.ts
+++ questions/00119-medium-replaceall/template.ts
@@ -1,1 +1,3 @@
-type ReplaceAll<S extends string, From extends string, To extends string> = any
+type ReplaceAll<S extends string, From extends string, To extends string> = From extends '' ?
+  S : S extends `${infer F}${From}${infer Rest}` ?
+    `${F}${To}${ReplaceAll<Rest, From, To>}` : S
