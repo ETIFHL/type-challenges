d06d48f7ae026c130adbb5d75a03561343aef3bb
ETIFHL <ashetifhl@gmail.com>
2022-06-16 16:19:52 +0800

    00108-medium-trim

--- questions/00108-medium-trim/template.ts
+++ questions/00108-medium-trim/template.ts
@@ -1,1 +1,3 @@
-type Trim<S extends string> = any
+type EmptySpaceChar = ' ' | '\n' | '\t'
+
+type Trim<S extends string> = S extends `${EmptySpaceChar}${infer R}` | `${infer R}${EmptySpaceChar}` ? Trim<R> : S
