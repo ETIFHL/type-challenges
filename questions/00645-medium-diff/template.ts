429cc467acfb58e615e00fecc0d6186140a07b82
ETIFHL <ashetifhl@gmail.com>
2022-06-23 18:03:36 +0800

    00645-medium-diff

--- questions/00645-medium-diff/template.ts
+++ questions/00645-medium-diff/template.ts
@@ -1,1 +1,3 @@
-type Diff<O, O1> = any
+type Diff<O extends {}, O1 extends {}> = {
+  [key in (keyof O | keyof O1) as key extends (keyof O & keyof O1) ? never : key]: key extends keyof O1 ? O1[key] : key extends keyof O ? O[key] : never
+}
