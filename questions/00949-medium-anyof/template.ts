d3e74dadf004d74fa8defbf4bc6edb19e32241f6
ETIFHL <ashetifhl@gmail.com>
2022-07-08 18:06:23 +0800

    00949-medium-anyof

--- questions/00949-medium-anyof/template.ts
+++ questions/00949-medium-anyof/template.ts
@@ -1,1 +1,3 @@
-type AnyOf<T extends readonly any[]> = any
+type isTrue<T> = T extends Record<any, never> | [] | 0 | '' | false ? true : false
+
+type AnyOf<T extends readonly any[]> = isTrue<T[number]> extends true | [] ? false : true
